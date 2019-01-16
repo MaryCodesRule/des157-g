d545ddf194ae1713e4aab2f40980a158605f2aef
Maryam Selassie <mselassie@ucdavis.edu>
2019-01-16 03:59:55 -0800

    hmtl

--- script-2.js
+++ script-2.js
@@ -9,14 +9,15 @@ function setup() {
   myCanvas.parent('mySketch');
 
   img = loadImage("images/banner.png"); //Had trouble putting the image in.
-  //img2 = loadImage("images/background_ice.png" "alt=background");
+
+  img = loadImage("images/banner-2.png");
 }
 
 function draw() {
   console.log('img');
 
     image(img, 0, 0);
-    //image(background,width,height)
+
 
   if (mouseIsPressed) { //when mouse is pressed on program it reacts
     fill(random(250), random(255), random(255));
