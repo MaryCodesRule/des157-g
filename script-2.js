74cc953044aac053e6aaf9e7b900700ddd42400e
Maryam Selassie <mselassie@ucdavis.edu>
2019-01-16 02:35:56 -0800

    Update script-2.js

--- script-2.js
+++ script-2.js
@@ -14,8 +14,9 @@ function setup() {
 
 function draw() {
   console.log('img');
-    image(img2, 0, 0);
+
     image(img, 0, 0);
+    image(img2, 0, 0);
   if (mouseIsPressed) { //when mouse is pressed on program it reacts
     fill(random(250), random(255), random(255));
     ellipse(165, 110, 55, 55);
