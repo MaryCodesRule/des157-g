d85c136861f68c107bddcd3df59139fb79ce1920
Maryam Selassie <mselassie@ucdavis.edu>
2019-01-28 01:55:59 -0800

    studio1

---
+++ studio1/script.js
@@ -0,0 +1,11 @@
+console.log('reading script');
+// function validateForm() {
+  // var x = document.forms["myForm"]["fname"].value;
+
+  // if (x == "") {
+    // alert("Name must be filled out");
+    // return false;
+  // }
+
+// } I am not sure how to use this so I will not put the code into
+// action
