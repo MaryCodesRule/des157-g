43a28165c55a796c4c534e4be3e8011f7543e4c1
Maryam Selassie <mselassie@ucdavis.edu>
2019-02-06 09:14:10 -0800

    add studio 2

--- studio2/script.js
+++ studio2/script.js
@@ -4,28 +4,26 @@ console.log('reading script');
 
 
 
-// var wallet = document.querySelector("wallet");
-//
-// wallet.addEventListener("mousedown", function() {
+
+// var wallet.addEventListener("mousedown", function() {
 //   console.log("mousedown on button");
-//   button.innerHTML = "Mullah Malla Chasin Change!";
-// });
+//  button.innerHTML = "Mullah Malla Chasin Change!";
+//   });
 // find id
-var wallet = document.getElementById('wallet')
-wallet.addEventListener("mousedown", function() {
-console.log("mousedown on button");
+ var wallet = document.getElementById('wallet')
+ wallet.addEventListener("mousedown", function() {
+ console.log("mousedown on button");
  wallet.innerHTML = "Mullah Malla Chasin Change!";
-});
+ });
 
 
-var pallete = document.getElementById('pallete')
+ var pallete = document.getElementById('pallete')
 pallete.addEventListener("mousedown", function() {
-console.log("mousedown on button");
- pallete.innerHTML = "When I have time...but it's the prettiest decoration!";
-});
-
-var water = document.getElementById('water')
+ console.log("mousedown on button");
+  pallete.innerHTML = "When I have time...but it's the prettiest decoration!";
+ });
+ var water = document.getElementById('water')
 water.addEventListener("mousedown", function() {
 console.log("mousedown on button");
  water.innerHTML = "Gotta Stay Hydrated!";
-});
+  });
