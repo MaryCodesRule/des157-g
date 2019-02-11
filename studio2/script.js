003e5146e8e2972c9ecaf8cbf191f754798cddef
Maryam Selassie <mselassie@ucdavis.edu>
2019-02-11 08:47:45 -0800

    Update script.js

--- studio2/script.js
+++ studio2/script.js
@@ -3,10 +3,7 @@ console.log('reading script');
 
 
 
-// var wallet.addEventListener("mousedown", function() {
-//   console.log("mousedown on button");
-//  button.innerHTML = "Mullah Malla Chasin Change!";
-//   });
+
 // find id
 var wallet = document.getElementById('wallet')
 wallet.addEventListener("mousedown", function() {
