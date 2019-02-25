89593f59910262faf26fc76c7e12f26fd8d799be
Maryam Selassie <mselassie@ucdavis.edu>
2019-02-25 04:21:58 -0800

    studio4 done

--- studio4/script.js
+++ studio4/script.js
@@ -7,11 +7,34 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+  ev.dataTransfer.setData("image", ev.target.id);
 }
 
 function drop(ev) {
   ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
+  var data = ev.dataTransfer.getData("image");
   ev.target.appendChild(document.getElementById(data));
+}
+
+// var drag1 = document.getElementById('drag1')
+// drag1.addEventListener("click", function() {
+//   console.log("click on text");
+//   drag1.innerHTML = "Mullah Malla Chasin Change! What is a wallet? A carrier of DREAMS. Money may not buy them, but they sure do help!!!!";
+// });
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+
+}
+function myFunction1() {
+var popup1 = document.getElementById("myPopup1");
+popup1.classList.toggle("show");
+}
+function myFunction2() {
+var popup2 = document.getElementById("myPopup2");
+popup2.classList.toggle("show");
+}
+function myFunction3() {
+var popup3 = document.getElementById("myPopup3");
+popup3.classList.toggle("show");
 }
